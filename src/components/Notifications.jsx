41590a8f5535436abfe3df292660c2c9fe7dc2b2
nextgen-2025 <203623776+nextgen-2025@users.noreply.github.com>
2025-05-13 16:17:56 +0530

    Update Notifications.jsx

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -104,6 +104,7 @@ const Notifications = () => {
         isRead: memo.isRead || false,
         message: memo.message,
         date: new Date(memo.createdAt),
+        // date: memo.date,
         createdAt: memo.createdAt,
         departmentName: memo.departments[0]?.departmentName || 'General',
       }));
@@ -116,6 +117,7 @@ const Notifications = () => {
         isRead: apt.isRead || false,
         doctorId: apt.doctorId,
         date: new Date(apt.createdAt),
+        date: apt.date,
         time: apt.time,
         createdAt: apt.createdAt
       }));
@@ -211,6 +213,16 @@ const Notifications = () => {
                         {notification.type === 'memo' 
                           ? new Date(notification.createdAt).toLocaleString()
                           : `${new Date(notification.date).toLocaleString()}`
+                        }
+                      </p>
+                      <p className="text-xs text-gray-500 mt-1">
+                        {notification.type === 'appointment' 
+                          ? `Slot Timings: ${new Date(notification.date).toLocaleDateString('en-US', {
+                              day: 'numeric',
+                              month: 'short',
+                              year: 'numeric'
+                            })} at ${notification.time}`
+                          : null
                         }
                       </p>
                       <p className="text-xs text-gray-500 mt-1">
